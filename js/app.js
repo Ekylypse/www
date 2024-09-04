bfd0a0fe4079ae09d1acb2f99be360400a385fc5
Anthony Dillavou <ekylypse@gmail.com>
2024-09-04 07:34:28 -0500

    Update app.js test enter/leave

--- js/app.js
+++ js/app.js
@@ -14,8 +14,8 @@ var stopButton = document.getElementById("stopButton");
 var pauseButton = document.getElementById("pauseButton");
 
 //add events to those 2 buttons
-recordButton.addEventListener("mousedown", startRecording);
-recordButton.addEventListener("mouseup", stopRecording);
+recordButton.addEventListener("mouseenter", startRecording);
+recordButton.addEventListener("mouseleave", stopRecording);
 stopButton.addEventListener("click", stopRecording);
 pauseButton.addEventListener("click", pauseRecording);
 
