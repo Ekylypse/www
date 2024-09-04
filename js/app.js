f28e19e4806a8dddfabbef64725c4299de962973
Anthony Dillavou <ekylypse@gmail.com>
2024-09-04 07:39:12 -0500

    Update app.js remove auto upload

--- js/app.js
+++ js/app.js
@@ -148,7 +148,7 @@ function createDownloadLink(blob) {
 	var upload = document.createElement('a');
 	upload.href="#";
 	upload.innerHTML = "Upload";
-	//upload.addEventListener("click", function(event){
+	upload.addEventListener("click", function(event){
 		  var xhr=new XMLHttpRequest();
 		  xhr.onload=function(e) {
 		      if(this.readyState === 4) {
@@ -159,7 +159,7 @@ function createDownloadLink(blob) {
 		  fd.append("audio_data",blob, filename);
 		  xhr.open("POST","upload.php",true);
 		  xhr.send(fd);
-	//})
+	})
 	li.appendChild(document.createTextNode (" "))//add a space in between
 	li.appendChild(upload)//add the upload link to li
 
